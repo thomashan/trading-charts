7e1c26f0e08887fb7857fe60204ed05776f96dc7
Thomas Han <thomas.han@live.com>
2020-09-29 18:59:25 +1000

    issue-7: added ohlc byte watcher test

---
+++ src/bytewatcher/java/io/github/thomashan/tradingchart/ohlc/OhlcBwTest.java
@@ -0,0 +1,36 @@
+package io.github.thomashan.tradingchart.ohlc;
+
+import io.github.thomashan.tradingchart.ByteWatcherRegressionTestHelper;
+import io.github.thomashan.tradingchart.price.BidAsk;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+
+public class OhlcBwTest {
+    private final long warmUpIterations = 1000;
+    private final long iterations = 1000;
+    private ByteWatcherRegressionTestHelper byteWatcherRegressionTestHelper;
+
+    @BeforeEach
+    void setUp() {
+        byteWatcherRegressionTestHelper = new ByteWatcherRegressionTestHelper();
+    }
+
+    @Test
+    void testNew() {
+        Runnable runnable = () -> {
+            Ohlc ohlc = new Ohlc<BidAsk>();        // 48 bytes
+            ohlc.setDateTime(ZonedDateTime.now()); // 264 bytes
+            ohlc.setOpen(new BidAsk(1, 1.1));      // 32 bytes
+            ohlc.setHigh(new BidAsk(1, 1.1));      // 32 bytes
+            ohlc.setLow(new BidAsk(1, 1.1));       // 32 bytes
+            ohlc.setClose(new BidAsk(1, 1.1));     // 32 bytes
+            ohlc.setVolume(1);
+        };
+
+        byteWatcherRegressionTestHelper
+                .warmUp(runnable, warmUpIterations)
+                .testAllocationNotExceeded(runnable, 440, 1);
+    }
+}
