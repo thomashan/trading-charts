721b04a2f0a5aaa85b2269efb16c400a17264faf
Thomas Han <thomas.han@live.com>
2023-02-09 01:32:10 +1100

    get rid of warnings

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module trading.charts.app {
+    requires transitive javafx.graphics;
     requires javafx.controls;
-    requires javafx.graphics;
     requires trading.charts.domain;
     requires trading.charts.ui;
+
+    exports io.github.thomashan.trading.charts.app;
 }
